0926f1ea3fb6f05ba98b4ba387a876f727d465f0
DevDataOrg <48391348+DevDataOrg@users.noreply.github.com>
2019-03-11 20:24:33 +0500

    Update ParseJSONSwift.swift

--- ParseJSONSwift.swift
+++ ParseJSONSwift.swift
@@ -66,20 +66,22 @@ func testSession () {
 func getForWeb () {
     guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
     let session = URLSession.shared
-    session.dataTask(with: url) { (data, response, error) in 
+    session.dataTask(with: url) { (data, response, error) in
         if let response = response {
             print(response)
         }
         guard let data = data else {return}
         print(data)
         do {
-                let json = try JSONSerialization().jsonObject(with: data, options: [])
-                print(json)
-            } catch  {
-                print(error)
-            }
-    }.resume()   
+            let json = try JSONSerialization.jsonObject(with: data, options: [])
+            print(json)
+        } catch  {
+            print(error)
+        }
+        }.resume()
 }
+
+getForWeb()
 
 
 //пример POST запроса 
@@ -89,22 +91,24 @@ func postForWeb () {
     var request = URLRequest(url: url)
     request.httpMethod = "POST"
     request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
+    
     guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return}
     request.httpBody = httpBody
     let session = URLSession.shared
-    session.dataTask(with: request) { (data, response, error) } in 
+    session.dataTask(with: request) { (data, response, error) in
         if let response = response {
             print(response)
-        }
-
-        guard let data = data else {return}
-        do {
-            let json = try JSONSerialization.jsonObject(with: data, options: [])
-            print(json)
-        } catch {
-            print(error)
-        }
-    }.resume() 
+    }
+        
+    guard let data = data else {return}
+    do {
+        let json = try JSONSerialization.jsonObject(with: data, options: [])
+        print(json)
+    } catch {
+        print(error)
+    }
+  }.resume()
 }
+
+postForWeb()
 
