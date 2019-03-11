9f4a643d03dd0b5d2ac4098d4bc31e691e357656
DevDataOrg <48391348+DevDataOrg@users.noreply.github.com>
2019-03-11 16:32:58 +0500

    Update ParseJSONSwift.swift

--- ParseJSONSwift.swift
+++ ParseJSONSwift.swift
@@ -62,3 +62,25 @@ func testSession () {
     
 }
 
+//пример GET запроса и получения ответа
+func getForWeb () {
+    guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos") else { return }
+    let session = URLSession.shared
+    session.dataTask(with: url) { (data, response, error) in 
+        if let response = response {
+            print(response)
+        }
+        guard let data = data else {return}
+        print(data)
+        do {
+                let json = try JSONSerialization().jsonObject(with: data, options: [])
+                print(json)
+            } catch  {
+                print(error)
+            }
+    }.resume()   
+}
+
+
+//пример POST запроса 
+
